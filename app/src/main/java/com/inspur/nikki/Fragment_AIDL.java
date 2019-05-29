6965834b4a5a9dfc4378ac8b29c9244a01ca6b66
FaceStudio <whospeaker@gmail.com>
2019-05-29 18:35:31 +0800

    增加aidl功能模块

--- app/src/main/java/com/inspur/nikki/Fragment_AIDL.java
+++ app/src/main/java/com/inspur/nikki/Fragment_AIDL.java
@@ -1,34 +1,37 @@
 package com.inspur.nikki;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Fragment_AIDL extends Fragment {
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
+
+    private TextView tv_show;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.layout_aidl, container, false);
-
-
+        tv_show = (TextView) view.findViewById(R.id.tv_show);
+        tv_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(),AIDLAcitivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
-
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 }
--- benny/src/main/java/com/inspur/benny/MainActivity.java
+++ benny/src/main/java/com/inspur/benny/MainActivity.java
@@ -1,6 +1,12 @@
 package com.inspur.benny;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +14,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.inspur.nikki.IMyAidlInterface;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IMyAidlInterface mIMyAidlInterface;
+
+    private Button bt_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +31,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bt_login = findViewById(R.id.bt_login);
+        bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                if (mIMyAidlInterface != null) {
+                    try {
+                        mIMyAidlInterface.login("nikki", "123456");
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
+
+        Intent intent = new Intent();
+        // 服务端AndroidManifest.xml文件该Service所配置的action
+        intent.setAction("com.inspur.nikki");
+        // Service所在的包名
+        intent.setPackage("com.inspur.nikki");
+        bindService(intent, new ConnectCallBack(), Context.BIND_AUTO_CREATE);
+
     }
+
+
+
+    public void login(View view) {
+        if (mIMyAidlInterface != null) {
+            try {
+                mIMyAidlInterface.login("nikki", "123456");
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class ConnectCallBack implements ServiceConnection {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mIMyAidlInterface = IMyAidlInterface.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mIMyAidlInterface = null;
+        }
+    }
+
+
+
+
 
 }
