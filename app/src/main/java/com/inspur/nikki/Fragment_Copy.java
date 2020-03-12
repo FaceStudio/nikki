87325559ed77dd22e40982b50317e5f8a1c3a3f0
FaceStudio <whospeaker@gmail.com>
2020-03-12 15:40:17 +0800

    增加多版本打包功能

--- app/src/main/java/com/inspur/nikki/Fragment_Copy.java
+++ app/src/main/java/com/inspur/nikki/Fragment_Copy.java
@@ -7,16 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 public class Fragment_Copy extends Fragment {
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
+
+    private Button start;
+    private TextView output;
+    private WebSocket mSocket;
 
 
     @Nullable
@@ -27,15 +39,28 @@ public class Fragment_Copy extends Fragment {
         String path = "/mnt/sdcard/";
         copy(getContext(),"1.jpg",path,"chenzhen.jpg");
 
+        start = (Button) view.findViewById(R.id.start);
+        output = (TextView) view.findViewById(R.id.output);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OkHttpClient mOkHttpClient = new OkHttpClient.Builder()
+                        .readTimeout(3, TimeUnit.SECONDS)//设置读取超时时间
+                        .writeTimeout(3, TimeUnit.SECONDS)//设置写的超时时间
+                        .connectTimeout(3, TimeUnit.SECONDS)//设置连接超时时间
+                        .build();
+
+                Request request = new Request.Builder().url("ws://echo.websocket.org").build();
+                EchoWebSocketListener socketListener = new EchoWebSocketListener();
+                mOkHttpClient.newWebSocket(request, socketListener);
+                mOkHttpClient.dispatcher().executorService().shutdown();
+            }
+        });
+
         return view;
     }
 
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     private void copy(Context myContext, String ASSETS_NAME,
                       String savePath, String saveName) {
@@ -60,5 +85,70 @@ public class Fragment_Copy extends Fragment {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private final class EchoWebSocketListener extends WebSocketListener {
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            super.onOpen(webSocket, response);
+            mSocket = webSocket;
+            String openid = "1";
+            //连接成功后，发送登录信息
+            String message = "{\"type\":\"login\",\"user_id\":\""+openid+"\"}";
+            mSocket.send(message);
+            mSocket.send("welcome");
+            mSocket.send(ByteString.decodeHex("adef"));
+            mSocket.close(1000, "再见");
+            output("连接成功！");
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            super.onMessage(webSocket, bytes);
+            output("receive bytes:" + bytes.hex());
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+            super.onMessage(webSocket, text);
+            output("receive text:" + text);
+            //收到服务器端发送来的信息后，每隔25秒发送一次心跳包
+            final String message = "{\"type\":\"heartbeat\",\"user_id\":\"heartbeat\"}";
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mSocket.send(message);
+                }
+            },25000);
+        }
+
+        @Override
+        public void onClosed(WebSocket webSocket, int code, String reason) {
+            super.onClosed(webSocket, code, reason);
+            output("closed:" + reason);
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            super.onClosing(webSocket, code, reason);
+            output("closing:" + reason);
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+            super.onFailure(webSocket, t, response);
+            output("failure:" + t.getMessage());
+        }
+    }
+
+    private void output(final String text) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                output.setText(output.getText().toString() + "\n\n" + text);
+            }
+        });
     }
 }
