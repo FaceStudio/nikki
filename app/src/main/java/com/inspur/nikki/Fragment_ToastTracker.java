464a4b0c03d964462347708079c562e75324a59f
Nikki <whospeaker@gmail.com>
2020-08-07 15:56:40 +0800

    优化toast检测反馈弹窗

--- app/src/main/java/com/inspur/nikki/Fragment_ToastTracker.java
+++ app/src/main/java/com/inspur/nikki/Fragment_ToastTracker.java
@@ -8,26 +8,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
 import java.util.Iterator;
 
 public class Fragment_ToastTracker extends Fragment {
 
     String SERVICE_ID = "com.inspur.nikki/.ToastTrackerService";
-
+    private AppCompatButton setting;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.layout_toasttracker, container, false);
+        setting = (AppCompatButton) view.findViewById(R.id.setting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.settings.ACCESSIBILITY_SETTINGS");
+                startActivity(intent);
+            }
+        });
 
         return view;
-
     }
 
 
@@ -35,16 +44,15 @@ public class Fragment_ToastTracker extends Fragment {
     public void onResume() {
         super.onResume();
 
-        if (!isAccessibilityEnabled(getContext(), this.SERVICE_ID)){
-            Intent intent = new Intent("android.settings.ACCESSIBILITY_SETTINGS");
-            startActivity(intent);
+        if (!isAccessibilityEnabled(getContext(), this.SERVICE_ID)) {
+            Toast.makeText(getContext(), "请跳转到辅助功能进行设置！", Toast.LENGTH_LONG).show();
         }
     }
 
     public static boolean isAccessibilityEnabled(Context paramContext, String paramString) {
-        @SuppressLint("WrongConstant") Iterator<AccessibilityServiceInfo> iterator = ((AccessibilityManager)paramContext.getSystemService("accessibility")).getEnabledAccessibilityServiceList(-1).iterator();
+        @SuppressLint("WrongConstant") Iterator<AccessibilityServiceInfo> iterator = ((AccessibilityManager) paramContext.getSystemService("accessibility")).getEnabledAccessibilityServiceList(-1).iterator();
         while (iterator.hasNext()) {
-            if (paramString.equals(((AccessibilityServiceInfo)iterator.next()).getId()))
+            if (paramString.equals(((AccessibilityServiceInfo) iterator.next()).getId()))
                 return true;
         }
         return false;
--- app/src/main/java/com/inspur/nikki/ToastTrackerService.java
+++ app/src/main/java/com/inspur/nikki/ToastTrackerService.java
@@ -3,8 +3,15 @@ package com.inspur.nikki;
 import android.accessibilityservice.AccessibilityService;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ToastTrackerService extends AccessibilityService {
 
@@ -12,15 +19,17 @@ public class ToastTrackerService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
 
-        String str = (String)accessibilityEvent.getPackageName();
-        if (!str.equals(getPackageName()) && !(accessibilityEvent.getParcelableData() instanceof android.app.Notification)) {
+        String str = (String) accessibilityEvent.getPackageName();
+        if (!(accessibilityEvent.getParcelableData() instanceof android.app.Notification)) {
 
             try {
                 PackageManager packageManager = getPackageManager();
                 @SuppressLint("WrongConstant") String name = packageManager.getApplicationLabel(getPackageManager().getApplicationInfo(str, 128)).toString();
-                @SuppressLint("WrongConstant") String packagename  = getPackageManager().getApplicationInfo(str, 128).toString();
+                @SuppressLint("WrongConstant") String packagename = getPackageManager().getApplicationInfo(str, 128).toString();
 
-                Log.i("Nikki","package name:"+packagename);
+                Log.i("Nikki", "package name:" + packagename);
+
+                showNotification(drawable2Bitmap(packageManager.getApplicationIcon(str)), name, packagename, getNotiID());
 
                 return;
             } catch (PackageManager.NameNotFoundException nameNotFoundException) {
@@ -32,6 +41,43 @@ public class ToastTrackerService extends AccessibilityService {
     @Override
     public void onInterrupt() {
 
+    }
+
+    private static final AtomicInteger c = new AtomicInteger(0);
+
+    public static int getNotiID() {
+        return c.incrementAndGet();
+    }
+
+    @SuppressLint("WrongConstant")
+    public static Bitmap drawable2Bitmap(Drawable paramDrawable) {
+        Bitmap.Config config;
+        int i = paramDrawable.getIntrinsicWidth();
+        int j = paramDrawable.getIntrinsicHeight();
+        if (paramDrawable.getOpacity() != -1) {
+            config = Bitmap.Config.ARGB_8888;
+        } else {
+            config = Bitmap.Config.RGB_565;
+        }
+        Bitmap bitmap = Bitmap.createBitmap(i, j, config);
+        Canvas canvas = new Canvas(bitmap);
+        paramDrawable.setBounds(0, 0, paramDrawable.getIntrinsicWidth(), paramDrawable.getIntrinsicHeight());
+        paramDrawable.draw(canvas);
+        return bitmap;
+    }
+
+    public void showNotification(Bitmap paramBitmap, String paramString1, String paramString2, int paramInt) {
+
+        NotificationCompat.Builder builder = (new NotificationCompat.Builder(getApplicationContext())).setSmallIcon(R.drawable.java).setLargeIcon(paramBitmap);
+        StringBuilder stringBuilder2 = new StringBuilder();
+        stringBuilder2.append("");
+        stringBuilder2.append(paramString1);
+        builder = builder.setContentTitle(stringBuilder2.toString());
+        StringBuilder stringBuilder1 = new StringBuilder();
+        stringBuilder1.append("Toast");
+        stringBuilder1.append(paramString2);
+        builder = builder.setContentText(stringBuilder1.toString()).setPriority(2);
+        NotificationManagerCompat.from(getApplicationContext()).notify(paramInt, builder.build());
     }
 
 }
